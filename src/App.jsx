47db411236318d36ff21259988a1ba07a8d026e1
shohag-stack <shohag4y@gmail.com>
2025-02-05 03:47:26 +0600

    staging first draft of calculator

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Box, Button, Input, SimpleGrid, VStack } from "@chakra-ui/react";
+
+const Calculator = () => {
+
+  const [input,setInput] = useState("")
+
+  const numbers = ["(",")","CE","%","7","8","9","/","4","5","6","*","1","2","3","-","0",".","=","+"]
+
+  const handleInput =  (value)=> {
+    if (value === "CE") {
+      setInput(input.slice(0, -1)); // Remove the last character
+    } else {
+      setInput((prev) => prev + value);
+    }
+  }
+
+  const clearInput = ()=>(
+    setInput("")
+  )
+
+  const calculateResult = ()=> {
+
+    try {
+      setInput(eval(input).toString())
+    }
+    catch {
+      setInput("Error");
+    }
+
+  }
+  
+
+  return (
+    <Box height='100vh' display='flex' justifyContent='center' alignItems='center'>
+      <VStack p={5} spacing={4} boxShadow="lg" borderRadius="md" maxW="sm" mx="auto">
+      <Input readOnly value={input} textAlign="right" fontSize="2xl" p={4} />
+      <SimpleGrid columns={4} spacing={2} w="full">
+      {
+        numbers.map((char,index)=>(
+
+          <Button key={index} m={1} bg="bg.muted" color="bg.inverted" onClick={()=> (char === "=" ? calculateResult() : handleInput(char))}>
+            {char}
+          </Button>
+
+        ))
+      }
+        
+      </SimpleGrid>
+    </VStack>
+    </Box>
+  );
+};
+
+export default Calculator;
